ece629b3cd15b6571af39837d199ea85c54b194d
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2010-11-21 23:37:11 +0000

    Started implementing CSS3 page flip - based on Roman Cortes's Pure CSS3 Page Flip Effect - implemented in pagefliptest.html for now...

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+
+    drawTree();
+
+    $('#frontpage').click(function() {
+        openCard();
+    });
+
+});
+
+function drawTree() {
+
+    var context = $('#tree')[0].getContext("2d");
+    context.moveTo(150, 0);
+    context.lineTo(200, 50);
+    context.lineTo(170, 50);
+    context.lineTo(250, 140);
+    context.lineTo(200, 140);
+    context.lineTo(280, 250);
+    context.lineTo(20, 250);
+    context.lineTo(100, 140);
+    context.lineTo(50, 140);
+    context.lineTo(130, 50);
+    context.lineTo(100, 50);
+    context.lineTo(150, 0);
+    context.strokeStyle='#406B2E';
+    context.stroke();
+    context.fillStyle='#5CD12E';
+    context.fill();
+
+}
+
+function openCard() {
+    
+    alert('Open card');
+
+}
