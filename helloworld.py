28a01179905fd09cad03e393a104d946e284f4ed
oshaughnessyp <oshaughnessyp@tglmweb-2.local>
2010-11-22 20:33:07 +0000

    Made it a Google App Engine project and some more tweaks to the pagefliptest HTML

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+
+#        self.response.headers['Content-Type'] = 'text/plain'
+#        self.response.out.write('Hello, webapp World!')
+
+         self.response.out.write(template.render('pagefliptest.html', None))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
